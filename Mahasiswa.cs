726d0f9ea51ef69d8746ad6c116b9dd5efdb1e78
adit <adit@gmail.com>
2019-03-19 12:03:28 +0700

    commit pertama

---
+++ Mahasiswa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatianClassDanObject
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("Nim : {0}", Nim);
+            Console.WriteLine("IPK : {0}", Ipk);
+            Console.WriteLine("Telah Melakukan Registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses krs\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatianClassDanObject
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+
+            Console.Write("Masukkan Nim Anda = ");
+            mhs.Nim = Console.ReadLine();
+            Console.Write("Masukkan Nama Anda = ");
+            mhs.Nama = Console.ReadLine();
+            Console.Write("Masukkan IPK Anda = ");
+            mhs.Ipk = float.Parse(Console.ReadLine());
+
+            mhs.Registrasi();
+            mhs.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
